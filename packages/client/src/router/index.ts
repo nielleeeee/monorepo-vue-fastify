5cd6f762f6081c2348170ce88ca2e3b9fa85e988
nielleeeee <jandnllplz@gmail.com>
2025-07-15 07:05:39 +0800

    chore: added workflow page

--- packages/client/src/router/index.ts
+++ packages/client/src/router/index.ts
@@ -33,6 +33,11 @@ const router = createRouter({
       name: 'create',
       component: () => import('../views/CreateItemVew.vue'),
     },
+    {
+      path: '/workflow',
+      name: 'workflow',
+      component: () => import('../views/WorkflowView.vue'),
+    },
   ],
 })
 
