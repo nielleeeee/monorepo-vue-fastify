fc918da61be6e8a5a75b7541a4786cc836db517f
nielleeeee <jandnllplz@gmail.com>
2025-07-16 07:05:30 +0800

    feat: workflow init

---
+++ packages/workflow/src/index.ts
@@ -0,0 +1,91 @@
+import { Hono } from "hono";
+import { logger } from "hono/logger";
+import { poweredBy } from "hono/powered-by";
+import { cors } from "hono/cors";
+import { authMiddleware } from "./middleware";
+import { Params, WorkflowResponse } from "./types";
+import { TestWorkflow } from "./workflow/sampleWorkflow";
+
+const app = new Hono<{ Bindings: Env }>();
+
+app.use(logger());
+app.use(poweredBy());
+
+app.use(
+    "*",
+    cors({
+        origin: "http://localhost:5173",
+    })
+);
+
+app.get("/", (c) => {
+    return c.text("Hello Hono!");
+});
+
+app.post("/test-workflow", authMiddleware, async (context) => {
+    try {
+        const body = await context.req.json<Params>();
+
+        if (!body || !body.name) {
+            return context.json(
+                { success: false, message: "Missing body" },
+                400
+            );
+        }
+
+        let workflow = context.env.TEST_WORKFLOW;
+
+        const workflowCreate = await workflow.create({
+            id: "workflow-run-" + crypto.randomUUID(),
+            params: body,
+        });
+
+        return context.json({
+            success: true,
+            message: "Workflow started",
+            id: workflowCreate.id,
+        });
+    } catch (error) {
+        console.error(error);
+        return context.json(
+            { success: false, message: "Failed to start workflow" },
+            500
+        );
+    }
+});
+
+app.get("/test-workflow/:id", authMiddleware, async (context) => {
+    try {
+        const { id } = context.req.param();
+
+        const instance = await context.env.TEST_WORKFLOW.get(id);
+
+        const status = await instance.status();
+
+        if (status.status === "complete") {
+            return context.json({
+                success: true,
+                message: "Workflow completed",
+                data: status.output,
+            });
+        }
+
+        return context.json({
+            success: true,
+            message: "Workflow in progress",
+            data: status,
+        });
+    } catch (error) {
+        console.error(error);
+        return context.json(
+            { success: false, message: "Failed to get workflow status" },
+            500
+        );
+    }
+});
+
+export { TestWorkflow };
+
+export default {
+    fetch: app.fetch,
+};
