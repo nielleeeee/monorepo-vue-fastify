7611457e536c18a2a57f51f549e3e83a5b27e6d9
nielleeeee <jandnllplz@gmail.com>
2025-07-21 06:18:03 +0800

    feat: fetch data from backend

--- packages/client/src/views/SingleItemView.vue
+++ packages/client/src/views/SingleItemView.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
-import { reactive, computed, watch, ref } from 'vue'
+import { reactive, computed, watch, ref, onMounted } from 'vue'
+import { storeToRefs } from 'pinia'
 import { useStoreItem } from '@/stores/storeItem'
 import BaseModal from '@/components/BaseModal.vue'
+import { trpc } from '@/trpc'
+import { useRouter } from 'vue-router'
+
+import type { Item } from '@/types/itemType'
 
 const props = defineProps<{
   id: string
@@ -12,39 +17,88 @@ const formState = reactive({
   price: 0,
 })
 
-const isOpen = ref(false)
-
 const store = useStoreItem()
+const { storeItem } = storeToRefs(store)
+const { editStoreItem, deleteStoreItem } = store
+const isOpen = ref(false)
+const isOpenDelete = ref(false)
+const isLoading = ref(false)
+const itemData = ref<Item | null>(null)
+const router = useRouter()
 
-const singleItem = computed(() => store.storeItem.find((item) => item.id === props.id))
+const fetchItem = async () => {
+  isLoading.value = true
 
-const handleSave = () => {
-  if (!singleItem.value) return
+  try {
+    const item = await trpc.getStoreItemById.query({ id: props.id })
 
-  console.log('Item to be updated: ', formState)
+    return item
+  } catch (error) {
+    console.error('Error fetching item:', error)
 
-  store.editStoreItem({
-    id: props.id,
-    name: formState.name,
-    price: formState.price,
-  })
-
-  // Close Modal
-  isOpen.value = false
-
-  console.log('Store Item: ', store.storeItem)
+    return null
+  }
 }
 
 watch(
-  singleItem,
-  (newItem) => {
-    if (!newItem) return
+  () => props.id,
+  async (newId) => {
+    if (!newId) return
 
-    formState.name = newItem.name
-    formState.price = newItem.price
+    const item = await fetchItem()
+
+    itemData.value = item
+
+    formState.name = item?.name || ''
+    formState.price = item?.price || 0
   },
   { immediate: true },
 )
+
+const handleSave = async (): Promise<void> => {
+  if (!itemData.value) return
+
+  try {
+    editStoreItem({
+      id: props.id,
+      name: formState.name,
+      price: formState.price,
+    })
+
+    const result = await trpc.updateStoreItem.mutate({
+      id: props.id,
+      name: formState.name,
+      price: formState.price,
+    })
+
+    // Save New Item To Ref
+    itemData.value = result
+
+    console.log('Item updated:', result)
+  } catch (error) {
+    console.error('Error updating item:', error)
+  } finally {
+    isOpen.value = false
+  }
+}
+
+const handleDelete = async (): Promise<void> => {
+  if (!itemData.value) return
+
+  try {
+    deleteStoreItem(itemData.value)
+
+    const result = await trpc.deleteStoreItem.mutate({ id: props.id })
+
+    console.log('Item deleted:', result)
+  } catch (error) {
+    console.error('Error Deleting Item: ', error)
+  } finally {
+    isOpenDelete.value = false
+
+    router.push('/shop')
+  }
+}
 </script>
 
 <template>
@@ -52,13 +106,17 @@ watch(
     <div>
       <h1>Single Item View</h1>
       <p>Item ID: {{ id }}</p>
-      <p>Item Name: {{ singleItem?.name }}</p>
-      <p>Item Price: {{ singleItem?.price }}</p>
+      <p>Item Name: {{ itemData?.name }}</p>
+      <p>Item Price: {{ itemData?.price }}</p>
     </div>
 
     <button @click="isOpen = true">Edit</button>
 
     <BaseModal :is-open="isOpen" @close="isOpen = false">
+      <template #header>
+        <h2>Edit Item</h2>
+      </template>
+
       <form @submit.prevent="handleSave">
         <label for="name">Name</label>
         <input type="text" id="name" name="name" v-model="formState.name" />
@@ -68,7 +126,22 @@ watch(
         <button type="submit">Edit</button>
       </form>
     </BaseModal>
+
+    <button @click="isOpenDelete = true">Delete</button>
+
+    <BaseModal :is-open="isOpenDelete" @close="isOpenDelete = false">
+      <template #header>
+        <h2>Delete Item</h2>
+      </template>
+
+      <p>Are you sure you want to delete this item?</p>
+
+      <div>
+        <button @click="isOpenDelete = false">Cancel</button>
+        <button @click="handleDelete">Delete</button>
+      </div>
+    </BaseModal>
   </section>
 </template>
 
-<style lang=""></style>
+<style></style>
