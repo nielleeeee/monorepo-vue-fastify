4f01ab63d296f8d376ce589ad1fc13a964112398
nielleeeee <jandnllplz@gmail.com>
2025-07-29 13:03:08 +0800

    feat: enhance TestWorkflow to send email and SMS notifications

--- packages/workflow/src/workflow/sampleWorkflow.ts
+++ packages/workflow/src/workflow/sampleWorkflow.ts
@@ -3,35 +3,61 @@ import {
     WorkflowEvent,
     WorkflowStep,
 } from "cloudflare:workers";
-import { Params } from "../types";
+import { sendEmail } from "../helper/email";
+import { sendSMS } from "../helper/sms";
+import { SampleWorkflowParams } from "../types";
 
-export class TestWorkflow extends WorkflowEntrypoint<Env, Params> {
-    async run(event: WorkflowEvent<Params>, step: WorkflowStep) {
+export class TestWorkflow extends WorkflowEntrypoint<
+    Env,
+    SampleWorkflowParams
+> {
+    async run(event: WorkflowEvent<SampleWorkflowParams>, step: WorkflowStep) {
         const { env } = this;
-        const { name } = event.payload;
+        const { name, email, phone, emailMessage, smsMessage, subject } =
+            event.payload;
 
-        if (!name) {
-            throw new Error("Missing name params");
+        if (!name || !email || !phone) {
+            throw new Error("Missing required params");
         }
 
+        const sendEmailProps = {
+            email,
+            name,
+            subject: subject ?? "Sample Workflow Email",
+            emailMessage: emailMessage ?? "This is the email body",
+        };
+
+        const sendSMSProps = {
+            phone,
+            name,
+            smsMessage: smsMessage ?? "This is the SMS body",
+        };
+        
+
         const first = await step.do("First step", async () => {
-            console.log("Console Log From Workflow | Step 1");
+            console.log("Console Log From Workflow | Step 1 | Starting");
+
             return "First step";
         });
-
+        
         const second = await step.do("Second step", async () => {
-            console.log("Console Log From Workflow | Step 2");
-            return "Second step";
+            console.log("Console Log From Workflow | Step 2 | Sending Email");
+
+            const sendEmailResult = await sendEmail(sendEmailProps, env);
+
+            return sendEmailResult;
         });
 
         const third = await step.do("Third step", async () => {
-            console.log("Console Log From Workflow | Step 3");
-            return "Third step";
+            console.log("Console Log From Workflow | Step 3 | Sending SMS");
+
+            const sendSMSResult = await sendSMS(sendSMSProps, env);
+
+            return sendSMSResult;
         });
 
         await step.do("Final step", async () => {
             console.log("Console Log From Workflow | Final step");
-            console.log("Name: ", name);
             console.log("Workflow Completed");
             return { first, second, third };
         });
