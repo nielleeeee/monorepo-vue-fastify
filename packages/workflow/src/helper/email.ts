da8ccf54f6e43abcd04b5f0d6731e2a637c642b5
nielleeeee <jandnllplz@gmail.com>
2025-07-31 11:43:45 +0800

    feat: add sendEmail function to handle email notifications

---
+++ packages/workflow/src/helper/email.ts
@@ -0,0 +1,27 @@
+import { Resend } from "resend";
+import { EmailParams } from "../types";
+
+const sendEmail = async (
+    { email, name, subject, emailMessage }: EmailParams,
+    env: Env
+) => {
+    const resend = new Resend(env.RESEND_API_KEY);
+
+    const { data, error } = await resend.emails.send({
+        from: "onboarding@resend.dev",
+        to: [email],
+        subject: subject ?? `Test Email For ${name}`,
+        // html: text,  
+        text: emailMessage ?? `This is the email body for ${name}`,
+    });
+
+    if (error) {
+        console.error("Error sending email:", error);
+        throw new Error("Error sending email");
+    }
+
+    console.log("Email sent:", data);
+    return data;
+};
+
+export { sendEmail };
