26770ad41a525b54dd7175c9b6a796c02c9a3a5b
nielleeeee <jandnllplz@gmail.com>
2025-11-17 07:35:49 +0800

    refactor: remove old Firebase initialization and create new configuration files for Firestore and Firebase SDK

---
+++ packages/server/src/config/firebase.ts
@@ -0,0 +1,6 @@
+import { initializeApp } from "firebase-admin/app";
+
+// ADC handle auth
+export const firebaseApp = initializeApp();
+
+console.log("Firebase Admin SDK initialized successfully!");
