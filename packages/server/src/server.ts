d1810924eb2f03863df8f53ac60dbdb552f742ff
nielleeeee <jandnllplz@gmail.com>
2025-11-17 07:36:15 +0800

    feat: enable logging in Fastify server and format code for consistency

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -3,24 +3,26 @@ import cors from "@fastify/cors";
 import { fastifyTRPCPlugin } from "@trpc/server/adapters/fastify";
 import { appRouter } from "./routes";
 
-const server = Fastify();
+const server = Fastify({
+    logger: true,
+});
 
 await server.register(cors, {
-  origin: "*", 
+    origin: "*",
 });
 
 await server.register(fastifyTRPCPlugin, {
-  prefix: "/trpc",
-  trpcOptions: { router: appRouter },
+    prefix: "/trpc",
+    trpcOptions: { router: appRouter },
 });
 
 const start = async () => {
-  try {
-    await server.listen({ port: 3000 });
-  } catch (err) {
-    server.log.error(err);
-    process.exit(1);
-  }
+    try {
+        await server.listen({ port: 3000 });
+    } catch (err) {
+        server.log.error(err);
+        process.exit(1);
+    }
 };
 
 start();
